8067336d8d7b668debec5febe44732e4a63b8edc
Mbeth <muradbzuneh@gmail.com>
2026-02-09 22:52:04 -0800

    first code for python

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Pyton_programming
+
+this repo is for payton programming when i learn in class and practice 
---
+++ class-1/payton1.py
@@ -0,0 +1,7 @@
+print("hello world");
+
+x=10
+y=20
+if x>y:
+    print("Hello Murad ")
+else:    print("Hello Payton")
