4ed74dcd342c5fdb167be4ce762220707bc10c79
Mbeth <muradbzuneh@gmail.com>
2026-03-03 13:19:07 -0800

    to understan basic syntax , indantation and running program

---
+++ assignment/tak1.py
@@ -0,0 +1,12 @@
+name = "Murad"
+dpt = "Software engineering"
+university = "Wollo university"
+hobbies ="i will build international multi paltform amd improve my skill by applayig some knolowge "
+msg = " this is the first assignment for a group using pyton"
+
+
+print(name)
+print(dpt)
+print(university)
+print(hobbies)
+print(msg)
---
+++ assignment/task2.py
@@ -0,0 +1,7 @@
+number =int(input("enter the number u want "))
+if number > 0:
+    print("Number is posetive")
+elif number == 0:
+    print("numeber is zero")
+elif number < 0 :
+  print("Number is negative")
