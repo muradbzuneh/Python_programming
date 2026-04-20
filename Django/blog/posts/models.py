de3fadc3eceb8595fd65ce09946f8809245bfef8
Mbeth <muradbzuneh@gmail.com>
2026-04-19 22:28:13 -0700

    create djongo and simple client page using html in html

--- Django/blog/posts/models.py
+++ Django/blog/posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    is_published = models.BooleanField(default=False)
+    published_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+       
--- Django/blog/posts/views.py
+++ Django/blog/posts/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 # Create your views here.
 def home(request):
-    return HttpResponse("<h1>Hello, World!</h1>")
+    context = {}
+    return render(request, 'home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>About Us</h1><p>This is the about page.</p>")
+    context = {}
+    return render(request, 'about_us.html', context)
 
 def contact(request):
     return HttpResponse("<h1>Contact Us</h1><p>This is the contact page.</p>")
